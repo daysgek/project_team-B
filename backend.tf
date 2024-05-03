a5aa6581acae394436adc9a41783d82c96ec38a4
daysgek <151466567+daysgek@users.noreply.github.com>
2024-05-03 02:32:39 +0100

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket = "fusisoft-terraform-backend-bucket"
+    key    = "infra_resources/terraform.tfstate"
+    region = "us-west-2"
+    dynamodb_table = "terraform-state-locking"
+  }
+}
